6c62bc378c68ad92ce2735073ad70591a209465c
Manuel Gómez <manutazo4@hotmail.es>
2017-11-02 00:33:59 +0100

    [FIX] solve problem whit not login

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,10 +7,8 @@ class AlbumsController < ApplicationController
   # GET /albums/1.json
   def show
     require 'will_paginate/array'
-    if  @album.public == false && user_signed_in?
-      if @album.public == false && current_user != @album.user
-        redirect_to root_path
-      end
+    if @album.public == false && current_user != @album.user
+      redirect_to root_path
     end
     @images = @album.images.paginate(page: params[:page], per_page: 20)
   end
