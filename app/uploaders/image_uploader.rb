33be4f164e59d66ccbc4a6e32e685cfce52cf5bf
Manuel Gómez <manutazo4@hotmail.es>
2017-11-02 00:45:58 +0100

    [FEATURE] add other size to images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
 
     version :medium do
-      process resize_to_limit: [150, 150]
+      process resize_to_limit: [120, 120]
     end
 
     version :thumb do
